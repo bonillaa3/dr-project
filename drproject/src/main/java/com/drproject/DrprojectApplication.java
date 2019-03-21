ef7f021071ba31df42e95704e8cc9b8dec223abd
bonillaa3 <31493966+bonillaa3@users.noreply.github.com>
2019-03-21 17:46:23 -0400

    Add files via upload

---
+++ drproject/src/main/java/com/drproject/DrprojectApplication.java
@@ -0,0 +1,13 @@
+package com.drproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrprojectApplication.class, args);
+	}
+
+}
