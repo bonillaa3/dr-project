0f535e266cfb2b485bf235c53546f5c5d975b783
Haley <hhodges10@msn.com>
2020-02-02 15:10:55 -0600

    initial commit

--- DrProfit/src/main/java/com/project/drprofit/DrprofitApplication.java
+++ DrProfit/src/main/java/com/project/drprofit/DrprofitApplication.java
@@ -1,13 +1,13 @@
-package com.drproject;
+package com.project.drprofit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DrprojectApplication {
+public class DrprofitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DrprojectApplication.class, args);
+		SpringApplication.run(DrprofitApplication.class, args);
 	}
 
 }
---
+++ DrProfit/src/test/java/com/project/drprofit/DrprofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.drprofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrprofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
